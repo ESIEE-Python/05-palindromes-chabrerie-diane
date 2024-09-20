cfdae924891834ee5490ccd685086966b4c50279
chabrerie-diane <diane.chabrerie@edu.esiee.fr>
2024-09-20 11:04:11 +0000

    exercice palindromes

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
+"""exercice palindrome"""
 #### Fonction secondaire
 
 
 def ispalindrome(p):
+    """verifie si palindrome"""
+    p = p.lower()
+    t = [['é','e'], ['è','e'], ['à','a'], ['ù','u'], ['ç','c'], ['ê','e'],
+    ['â','a'],['î','i'], ['ï', 'i'], ['ô', 'o'], ['ë', 'e']]
+    for couple in t:
+        mytable = str.maketrans(couple[0],couple[1])
+        p=p.translate(mytable)
+    p = p.replace(" ", "")
+    p = p.replace(",", "")
+    p = p.replace(".", "")
+    p = p.replace("'", "")
+    p = p.replace("!", "")
+    p = p.replace(":", "")
+    p = p.replace("?", "")
+    p = p.replace("-", "")
 
-    # votre code ici
-    
-    return False
+    lenth = len(p)
+    for i in range(lenth//2):
+        if p[i] != p[lenth -i -1]:
+            return False
+    return True
 
 #### Fonction principale
 
 
 def main():
-
+    """quelques tests"""
     # vos appels à la fonction secondaire ici
 
-    for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
+    for s in ["radar", "kayak", "level", "rotor", "civique", "deifie", "énorme"]:
         print(s, ispalindrome(s))
 
 
